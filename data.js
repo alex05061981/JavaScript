9065e3fbb146c9903ea739dd2757daeb305d3a77
Aleksandr <alex05061981@yandex.ru>
2023-01-28 19:09:42 +0300

    Добавил файл data.js

---
+++ data.js
@@ -0,0 +1,37 @@
+const datainfo = `[
+{
+    "name": "image1",
+    "copyright":"Dario Giannobile",
+    "date":"2023-01-28",
+    "explanation":"Comet-like plumes are blowing over the volcanic peaks of Mount Etna in this wintry mountain-and-skyscape from planet Earth. The stacked and blended combination of individual exposures recorded during the cold night of January 23, also capture naked-eye Comet ZTF just above Etna's snowy slopes. Of course increasing sunlight and the solar wind are responsible for the comet's greenish coma and broad dusty tail. This weekend Comet ZTF is dashing across northern skies between north star Polaris and the Big Dipper. From a dark site you can only just spot it as a fuzzy patch though. That's still an impressive achievement if you consider you are gazing at a visitor from the distant Oort cloud with your own eyes. But binoculars or a small telescope will make for an even more enjoyable view of this Comet ZTF in the coming days.   Comet ZTF Gallery: Notable Submissions to APOD",
+    "hdurl":"https://apod.nasa.gov/apod/image/2301/C2022E3ZTFMountEtna.jpg",
+    "media_type":"image",
+    "service_version":"v1",
+    "title":"Comet ZTF over Mount Etna",
+    "url":"https://apod.nasa.gov/apod/image/2301/C2022E3ZTFMountEtna1024.jpg",
+    "description": "Описание первой картинки"
+},
+{
+    "name": "image2",
+    "copyright":"Dario Giannobile",
+    "date":"2023-01-28",
+    "explanation":"Comet-like plumes are blowing over the volcanic peaks of Mount Etna in this wintry mountain-and-skyscape from planet Earth. The stacked and blended combination of individual exposures recorded during the cold night of January 23, also capture naked-eye Comet ZTF just above Etna's snowy slopes. Of course increasing sunlight and the solar wind are responsible for the comet's greenish coma and broad dusty tail. This weekend Comet ZTF is dashing across northern skies between north star Polaris and the Big Dipper. From a dark site you can only just spot it as a fuzzy patch though. That's still an impressive achievement if you consider you are gazing at a visitor from the distant Oort cloud with your own eyes. But binoculars or a small telescope will make for an even more enjoyable view of this Comet ZTF in the coming days.   Comet ZTF Gallery: Notable Submissions to APOD",
+    "hdurl":"https://i.vimeocdn.com/portrait/9705212_640x640",
+    "media_type":"image",
+    "service_version":"v1",
+    "title":"Comet ZTF over Mount Etna",
+    "url":"https://i.vimeocdn.com/portrait/9705212_640x640",
+    "description": "Описание второй картинки"
+},
+{
+    "name": "image3",
+    "copyright":"Dario Giannobile",
+    "date":"2023-01-28",
+    "explanation":"Comet-like plumes are blowing over the volcanic peaks of Mount Etna in this wintry mountain-and-skyscape from planet Earth. The stacked and blended combination of individual exposures recorded during the cold night of January 23, also capture naked-eye Comet ZTF just above Etna's snowy slopes. Of course increasing sunlight and the solar wind are responsible for the comet's greenish coma and broad dusty tail. This weekend Comet ZTF is dashing across northern skies between north star Polaris and the Big Dipper. From a dark site you can only just spot it as a fuzzy patch though. That's still an impressive achievement if you consider you are gazing at a visitor from the distant Oort cloud with your own eyes. But binoculars or a small telescope will make for an even more enjoyable view of this Comet ZTF in the coming days.   Comet ZTF Gallery: Notable Submissions to APOD",
+    "hdurl":"https://i.vimeocdn.com/portrait/9705212_640x640",
+    "media_type":"image",
+    "service_version":"v1",
+    "title":"Comet ZTF over Mount Etna",
+    "url":"https://i.vimeocdn.com/portrait/9705212_640x640",
+    "description": "Описание третий картинки"
+}]`
